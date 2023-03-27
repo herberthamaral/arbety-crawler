b724e0f3dd9a6633ed19066b1a3aefc64e3e3cb4
Herberth Amaral <herberthamaral@gmail.com>
2023-03-27 10:09:20 -0300

    Finaliza versão inicial do simulador

--- kelly_criterion.py
+++ kelly_criterion.py
@@ -19,8 +19,84 @@ def get_max_streak(db_slice):
         current = s
     return maxes
 
-def formula(probability_of_win: float = 17.0/36) -> float:
-    probability_of_loss = 1 - probability_of_win
-    proportion_of_win = 2
 
+def calc_probability(black, red, white):
+    """
+    Calcula a probabilidade da próxima jogada ser preto, branco ou vermelho
+    baseado na quantidade de cada um nas últimas 37 jogadas.
+
+    O equilíbrio perfeito é 18/37 para pretas e vermelhas e 1/37 para brancas.
+    """
+    total = float(black + white + red)
+    should_have_black_or_red = total * (18.0/37)
+    should_have_white = total * (1.0/37)
+    prob_black = (18.0 - (black - should_have_black_or_red))/37.0
+    prob_red = (18.0 - (red - should_have_black_or_red))/37.0
+    prob_white = (1.0 - (white - should_have_white))/37.0
+    return prob_black, prob_red, prob_white
+
+
+def formula(probability_of_win: float = 18.0/37, proportion_of_win: float = 2.0) -> float:
+    probability_of_loss = 1 - probability_of_win
     return probability_of_win - probability_of_loss / proportion_of_win
+
+
+def calc_bets_based_on_db_slice(db_slice):
+    black = sum([1 for c in db_slice if c == "black"])
+    red = sum([1 for c in db_slice if c == "red"])
+    white = sum([1 for c in db_slice if c == "white"])
+    p_black, p_red, p_white = calc_probability(black, red, white)
+    invest_black = formula(p_black)
+    invest_red = formula(p_red)
+    invest_white = formula(p_white, 20)
+    if invest_black < 0:
+        invest_red += (invest_black * -1)
+    if invest_red < 0:
+        invest_black += (invest_white * -1)
+    if invest_white < 0:
+        invest_white = 0
+    return invest_black, invest_red, invest_white
+
+
+def simulate_based_on_db(db):
+    slice_start = 0
+    slice_finish = 36
+    money = 100.0
+    len_db = len(db)
+    wins = 0
+    total_wins = 0
+    total_bets = 0
+    while slice_finish < len_db - 1 and money > 0:
+        db_slice = db[slice_start: slice_finish]
+        actual_roll = db[slice_finish + 1]
+        bb, br, bw = calc_bets_based_on_db_slice(db_slice)
+        gain, loss = 0, 0
+        money_to_bet = min(money, 100)
+        if actual_roll == "black":
+            gain = bb * money_to_bet * 2
+            loss = br * money_to_bet + bw * money_to_bet
+        if actual_roll == "red":
+            gain = br * money_to_bet * 2
+            loss = bb * money_to_bet + bw * money_to_bet
+        if actual_roll == "white":
+            gain = bw * money_to_bet * 20
+            loss = bb * money_to_bet + br * money_to_bet
+        money += gain
+        money -= loss
+        print(gain > loss, gain, loss, money)
+        wins += int(gain > loss)
+        total_wins += int(gain > loss)
+        if slice_start % 30 == 0:
+            input(f'wins: {wins}/30 (total: {total_wins}/{total_bets}):')
+            wins = 0
+        slice_start += 1
+        slice_finish += 1
+        total_bets += 1
+    return money
+
+def main():
+    db = read_db()
+    print(simulate_based_on_db(db))
+
+if __name__ == "__main__":
+    main()
