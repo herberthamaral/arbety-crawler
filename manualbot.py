f6ec8f85110a1565689d4f07f038c5e3f0132362
Herberth Amaral <herberthamaral@gmail.com>
2023-04-18 16:17:45 -0300

    Apostas em valores inteiros
    Também adiciona parâmetro para valor máximo de bet.

--- manualbot.py
+++ manualbot.py
@@ -6,13 +6,19 @@ from kelly_criterion import calc_bets_based_on_db_slice, read_db
 
 
 db = redis.Redis(host="localhost", port=6379, db=0)
+db.delete("arbety.bets")  # exclui bets antigos
 bets_db = read_db()
 db_slice = bets_db[len(bets_db)-30:]   # apenas os últimos 30 bets. assume continuidade
 # TODO: verificar se os últimos 30 estão contíguos
+
+# Parâmetros:
 money = 100
+max_bet = 500
+
 while money > 10:
     bb, br, bw = calc_bets_based_on_db_slice(db_slice)
-    print("Quanto apostar: black:", bb * money, " red: ", br * money)
+    money_to_bet = min(money, max_bet)
+    print("Quanto apostar: black:", int(bb * money_to_bet), " red: ", int(br * money_to_bet))
     # aguarda próximo bet
     _, raw_bet = db.blpop("arbety.bets")
     bet = json.loads(raw_bet.decode("utf-8"))
@@ -20,14 +26,14 @@ while money > 10:
     gain, loss = 0, 0
     actual_roll = bet[2]
     if actual_roll == "black":
-        gain = bb * money * 2  # TODO: nem sempre será x2. Pegar dados para saber quanto ganhamos
-        loss = br * money + bw * money
+        gain = int(bb * money_to_bet) * 2  # TODO: nem sempre será x2. Pegar dados para saber quanto ganhamos
+        loss = int(br * money_to_bet) + int(bw * money_to_bet)
     if actual_roll == "red":
-        gain = br * money * 2
-        loss = bb * money + bw * money
+        gain = int(br * money_to_bet) * 2
+        loss = int(bb * money_to_bet) + int(bw * money_to_bet)
     if actual_roll == "white":
         gain = 0  # Não apostamos em brancas (ainda)
-        loss = bb * money + br * money
+        loss = int(bb * money) + int(br * money)
     money += gain
     money -= loss
     print("Ganhamos ", gain, ". Perdemos ", loss, ". Saldo: ", money)
