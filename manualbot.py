4c4b22498671ec372f9cc47a8b905028d1db7c0c
Herberth Amaral <herberthamaral@gmail.com>
2023-04-18 15:43:59 -0300

    Finaliza versão inicial do manualbot

--- manualbot.py
+++ manualbot.py
@@ -1,41 +1,36 @@
 import sys
+import sqlite3
 import redis
 import json
-from kelly_criterion import calc_bets_based_on_db_slice
+from kelly_criterion import calc_bets_based_on_db_slice, read_db
 
 
 db = redis.Redis(host="localhost", port=6379, db=0)
-bets = []
-print("Carregando histórico de bets")
-for _ in range(30):
-    _, raw_bet = db.blpop("arbety.bets")
-    bet = json.loads(raw_bet.decode("utf-8"))
-    bets.append(bet)
-    sys.stdout.write(".")
-    sys.stdout.flush()
-print()
-# TODO: verificar se os últimos 30 estão em sequencia ou se tem gap
-while True:
-    money = 100
-    db_slice = [b[3] for b in bets]
+bets_db = read_db()
+db_slice = bets_db[len(bets_db)-30:]   # apenas os últimos 30 bets. assume continuidade
+# TODO: verificar se os últimos 30 estão contíguos
+money = 100
+while money > 10:
     bb, br, bw = calc_bets_based_on_db_slice(db_slice)
     print("Quanto apostar: black:", bb * money, " red: ", br * money)
     # aguarda próximo bet
     _, raw_bet = db.blpop("arbety.bets")
     bet = json.loads(raw_bet.decode("utf-8"))
+    print("Deu:", bet[2])
     gain, loss = 0, 0
-    actual_roll = bet["color"]
+    actual_roll = bet[2]
     if actual_roll == "black":
-        gain = bb * money_to_bet * 2  # TODO: nem sempre será x2. Pegar dados para saber quanto ganhamos
-        loss = br * money_to_bet + bw * money_to_bet
+        gain = bb * money * 2  # TODO: nem sempre será x2. Pegar dados para saber quanto ganhamos
+        loss = br * money + bw * money
     if actual_roll == "red":
-        gain = br * money_to_bet * 2
-        loss = bb * money_to_bet + bw * money_to_bet
+        gain = br * money * 2
+        loss = bb * money + bw * money
     if actual_roll == "white":
         gain = 0  # Não apostamos em brancas (ainda)
-        loss = bb * money_to_bet + br * money_to_bet
+        loss = bb * money + br * money
     money += gain
     money -= loss
     print("Ganhamos ", gain, ". Perdemos ", loss, ". Saldo: ", money)
-    bets.append([bet["id"], 0, bet["color"], bet["roll"]])
-    bets = bets[1:]
+    db_slice.append(bet[2])
+    db_slice = db_slice[1:]
+    print("-"*30)
